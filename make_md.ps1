3209b18d894c870b408c9f36b22ca741bc308504
Sakuma Kota <haruichi33333@gmail.com>
2022-11-29 20:17:04 +0900

    update

--- make_md.ps1
+++ make_md.ps1
@@ -1,4 +1,24 @@
+$TEMPLATE = "template.md"
+$ENCODING = "UTF8"
+
 $date = Get-Date -format yyyyMMddHHmmss
+
+$created = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+$updated = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+
 $filepath = $date + ".md"
-New-Item $filepath
-code $filepath
+Copy-Item $TEMPLATE $filepath
+
+(Get-Content $filepath -Encoding $ENCODING) | `
+foreach { $_ -replace "{UID}", $date } | `
+Set-Content $filepath -Encoding $ENCODING
+
+(Get-Content $filepath -Encoding $ENCODING) | `
+foreach { $_ -replace "{CREATED}", $created } | `
+Set-Content $filepath -Encoding $ENCODING
+
+(Get-Content $filepath -Encoding $ENCODING) | `
+foreach { $_ -replace "{UPDATED}", $updated } | `
+Set-Content $filepath -Encoding $ENCODING
+
+vim $filepath
---
+++ template.md
@@ -0,0 +1,10 @@
+---
+uid: {UID}
+title: 
+aliases: []
+created: {CREATED}
+updated: {UPDATED}
+tags: []
+draft: false
+---
+
